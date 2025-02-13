7c9b0818e0e6c4ed22882fddeebc7dc01da90a44
adcc662 <adcc662@gmail.com>
2025-02-12 21:27:01 -0600

    Fixing life cycle of the app in the app context

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
+from contextlib import asynccontextmanager
 from src.graphql.schema import schema
 from src.database.session import init_db
-app = FastAPI()
 
-@app.on_event("startup")
-async def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Manejo correcto del ciclo de vida de la app"""
     await init_db()
+    yield
+    print("App shutting down...")
+
+app = FastAPI(lifespan=lifespan)
 
 graphql_app = GraphQLRouter(schema)
 app.include_router(graphql_app, prefix="/graphql")
